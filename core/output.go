8deaa5ea16da69a92c881766c82336a4945c647c
cnlh <cnliuhe@126.com>
2020-09-18 10:19:02 +0800

    add ip type

--- core/output.go
+++ core/output.go
@@ -4,18 +4,18 @@ package core
 #cgo CFLAGS: -I./c/include
 #include "lwip/tcp.h"
 
-extern err_t output(struct pbuf *p);
+extern err_t output(struct pbuf *p,int t);
 
 err_t
 output_ip4(struct netif *netif, struct pbuf *p, const ip4_addr_t *ipaddr)
 {
-	return output(p);
+	return output(p,4);
 }
 
 err_t
 output_ip6(struct netif *netif, struct pbuf *p, const ip6_addr_t *ipaddr)
 {
-	return output(p);
+	return output(p,6);
 }
 
 void
@@ -32,15 +32,15 @@ import (
 	"errors"
 )
 
-var OutputFn func([]byte) (int, error)
+var OutputFn func([]byte,int) (int, error)
 
-func RegisterOutputFn(fn func([]byte) (int, error)) {
+func RegisterOutputFn(fn func([]byte,int) (int, error)) {
 	OutputFn = fn
 	C.set_output()
 }
 
 func init() {
-	OutputFn = func(data []byte) (int, error) {
+	OutputFn = func(data []byte,int) (int, error) {
 		return 0, errors.New("output function not set")
 	}
 }
